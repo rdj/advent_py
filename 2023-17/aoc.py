4391e5f6d2e4e822a0ee05a91bb9f6f953b80655
Ryan D Johnson <ryandjohnson@gmail.com>
2023-12-17 09:41:32 -0800

    2023-17 Part 2

--- 2023-17/aoc.py
+++ 2023-17/aoc.py
@@ -3,17 +3,6 @@
 from heapq import heappush, heappop
 from typing import NamedTuple
 
-PURPLE = '\033[95m'
-CYAN = '\033[96m'
-DARKCYAN = '\033[36m'
-BLUE = '\033[94m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-RED = '\033[91m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-RESET = '\033[0m'
-
 ExampleInput1 = """\
 2413432311323
 3215453535623
@@ -30,7 +19,13 @@ ExampleInput1 = """\
 4322674655533
 """
 
-import functools as ft
+ExampleInput2 = """\
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
+"""
 
 class Point(NamedTuple):
     x: int
@@ -44,9 +39,6 @@ class Point(NamedTuple):
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
-
-    def neighbors(self):
-        return [self + d for d in DIRECTIONS]
 
     def manhattan(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y)
@@ -72,94 +64,71 @@ class Maze:
         self.tiles = []
         for line in s.splitlines():
             self.tiles.append([int(_) for _ in line])
-        assert(len(self.tiles) == len(self.tiles[0]))
-        self.size = len(self.tiles)
+        self.height = len(self.tiles)
+        self.width = len(self.tiles[0])
 
     def in_bounds(self, p):
-        return (0 <= p.x < self.size and
-                0 <= p.y < self.size)
+        return (0 <= p.x < self.width and
+                0 <= p.y < self.height)
 
-    def neighbors(self, p):
-        return [n for n in p.neighbors() if self.in_bounds(n)]
-
-    def find_best_path(self):
+    def find_best_path(self, can_change_dir=0, must_change_dir=3):
         start = Point(0, 0)
-        goal = Point(self.size - 1, self.size - 1)
+        goal = Point(self.width - 1, self.height - 1)
 
-        best_known = {}#defaultdict(lambda: math.inf)
+        best_known = {}
         best_known[(start, DOWN, 0)] = 0
         best_known[(start, RIGHT, 0)] = 0
 
         visited = set()
 
         q = []
-        heappush(q, (0, 0, [start], DOWN, 0, []))
-        heappush(q, (0, 0, [start], RIGHT, 0, []))
+        heappush(q, (0, 0, start, DOWN, 0))
+        heappush(q, (0, 0, start, RIGHT, 0))
 
         # Heuristic cost for A-star is a best case _underestimate_ of the
         # remaining distance to goal. For 99% of grid-based pathfinding, this
-        # is just going to be the manhattan distance.
+        # is just going to be the manhattan distance. Leaving this comment
+        # because I *always* forget this and think this should be an upper
+        # rather than lower bound of the remaining cost.
         def h(p):
             return goal.manhattan(p)
 
         while q:
-            _, cost, path, facing, steps, moves = heappop(q)
-            pos = path[-1]
+            _, cost, pos, facing, steps = heappop(q)
+            key = (pos, facing, steps)
 
-            if (pos, facing, steps) in visited:
+            if key in visited:
                 continue
-            visited.add((pos, facing, steps))
+            visited.add(key)
 
             if pos == goal:
-                # self.dump(path, moves)
-                # print(cost)
-                # print(sum(self.tiles[p.y][p.x] for p in path[1:]))
-                # print(moves)
+                if steps < can_change_dir:
+                    continue
                 return cost
 
             dirs = NEXT_DIRS[facing]
             for d in dirs:
-                if steps == 3 and d == facing:
+                if steps < can_change_dir and d != facing:
+                    continue
+                if steps == must_change_dir and d == facing:
                     continue
 
                 n = pos + d
                 if not self.in_bounds(n):
                     continue
 
-                newpath = path + [n]
                 newcost = cost + self.tiles[n.y][n.x]
                 newsteps = 1
                 if d == facing:
                     newsteps = steps + 1
-                key = (n, d, newsteps)
-                if key in visited:
+                newkey = (n, d, newsteps)
+                if newkey in visited:
                     continue
-                if key not in best_known or newcost < best_known[key]:
+                if newkey not in best_known or newcost < best_known[newkey]:
                     best_known[key] = cost
-                    heappush(q, (newcost + h(n), newcost, newpath, d, newsteps, moves + [d]))
+                    heappush(q, (newcost + h(n), newcost, n, d, newsteps))
 
         raise Exception("goal not reached")
-
-    def dump(self, path, moves):
-        move_into = dict(zip(path[1:], moves))
-        for y in range(self.size):
-            for x in range(self.size):
-                p = Point(x, y)
-                if p in move_into:
-                    print(RED, end='')
-                    match move_into[p]:
-                        case Point(1, 0):
-                            print('>', end='')
-                        case Point(-1, 0):
-                            print('<', end='')
-                        case Point(0, 1):
-                            print('v', end='')
-                        case Point(0, -1):
-                            print('^', end='')
-                    print(RESET, end='')
-                else:
-                    print(self.tiles[y][x], end='')
-            print()
 
 
 def part1(s):
@@ -168,7 +137,8 @@ def part1(s):
 
 
 def part2(s):
-    return "TODO"
+    maze = Maze(s)
+    return maze.find_best_path(4, 10)
 
 
 def real_input():
@@ -177,20 +147,23 @@ def real_input():
 
 
 def run_all():
-    print("Example Part 1")
+    print("Example Part 1 (102)")
     print(part1(ExampleInput1))
 
     print()
-    print("Part 1")
-    import cProfile
-    cProfile.run('print(part1(real_input()))')
+    print("Part 1 (1039)")
+    print(part1(real_input()))
 
     print()
-    print("Example Part 2")
+    print("Example Part 2 (94)")
     print(part2(ExampleInput1))
 
     print()
-    print("Part 2")
+    print("Example Part 2 (71)")
+    print(part2(ExampleInput2))
+
+    print()
+    print("Part 2 (1201)")
     print(part2(real_input()))
 
 
