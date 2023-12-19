d25ae6064b36b0730e3841219d0353cfa75fa70e
Ryan D Johnson <ryandjohnson@gmail.com>
2023-12-18 17:05:26 -0800

    Template: import ansicolors

--- template/aoc.py
+++ template/aoc.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-
-PURPLE = '\033[95m'
-CYAN = '\033[96m'
-DARKCYAN = '\033[36m'
-BLUE = '\033[94m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-RED = '\033[91m'
-RESET = '\033[0m'
+from colors import color
 
 
 ExampleInput1 = """\
