2db92e3c5b988f3f0727bb324a23c67ed565e72f
Ryan D Johnson <ryandjohnson@gmail.com>
2024-12-01 22:28:08 -0800

    Note in the template some stuff I found useful in the past

--- template/aoc.py
+++ template/aoc.py
@@ -2,6 +2,37 @@
 
 from colors import color
 
+## Things I used in previous years
+
+# from collections import Counter
+# from collections import defaultdict
+# from collections import deque
+# from colors import color
+# from enum import Enum
+# from fractions import Fraction
+# from functools import cached_property
+# from functools import lru_cache
+# from functools import reduce
+# from functools import reduce, partial
+# from heapq import heappush, heappop
+# from itertools import pairwise
+# from math import prod
+# from more_itertools import chunked, sliding_window
+# from multiprocessing import Pool
+# from numpy import transpose
+# from operator import mul
+# from pathlib import Path
+# from typing import NamedTuple
+# import cProfile
+# import functools as ft
+# import itertools as it
+# import math
+# import networkx as nx
+# import operator as op
+# import pygraphviz as pgv
+# import re
+# import sympy
+
 
 ExampleInput1 = """\
 """
