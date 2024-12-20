fd02dc3ae5d6acc1a4e6b44212192e2a3b909380
Ryan D Johnson <ryandjohnson@gmail.com>
2024-12-19 22:03:24 -0800

    2024-20

---
+++ 2024-20/aoc.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env pypy3
+
+from collections import defaultdict
+from heapq import heappush, heappop
+from typing import NamedTuple
+
+
+ExampleInput1 = """\
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+"""
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    def __add__(self, other):
+        x, y = other
+        return Point(self.x + x, self.y + y)
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def neighbors(self):
+        return [self + d for d in DIRS]
+
+    def manhattan(self, other):
+        x0, y0 = self
+        x1, y1 = other
+        return abs(x0 - x1) + abs(y0 - y1)
+
+
+N = Point(0, -1)
+E = Point(1, 0)
+S = Point(0, 1)
+W = Point(-1, 0)
+
+DIRS = [N, E, S, W]
+
+class Grid:
+    def __init__(self, s):
+        self.grid = s.splitlines()
+        self.height = len(self.grid)
+        self.width = len(self.grid[0])
+
+        for y in range(self.height):
+            for x in range(self.width):
+                match self.grid[y][x]:
+                    case 'S':
+                        self.start = Point(x, y)
+
+                    case 'E':
+                        self.end = Point(x, y)
+
+    def __getitem__(self, p):
+        x, y = p
+        return self.grid[y][x]
+
+    def neighbors(self, p):
+        return [n for n in p.neighbors() if 0 <= n.y < self.height and 0 <= n.x < self.width]
+
+    def find_distances(self):
+        visited = set()
+        best_known = defaultdict(lambda: 2 << 31)
+
+        q = []
+        heappush(q, (0, self.end))
+        best_known[self.end] = 0
+
+        while q:
+            cost, pos = heappop(q)
+            key = pos
+
+            if key in visited:
+                continue
+            visited.add(key)
+
+            branches = self.neighbors(pos)
+            for newpos in branches:
+                if self[newpos] == '#':
+                    continue
+
+                newcost = cost + 1
+                newkey = newpos
+                bestcost = best_known[newkey]
+                if newcost > bestcost:
+                    continue
+
+                if newcost < bestcost:
+                    best_known[newkey] = newcost
+                    if newkey not in visited:
+                        heappush(q, (newcost, newpos))
+
+        return dict(best_known)
+
+    def find_cheats1(self):
+        cheats = defaultdict(int)
+        dists = self.find_distances()
+        for pos, cost in dists.items():
+            for n in [pos + d + d for d in DIRS]:
+                if n not in dists:
+                    continue
+                savings = cost - dists[n] - 2
+                if savings > 0:
+                    cheats[savings] += 1
+        return cheats
+
+    def find_cheats(self, cheatlen=2):
+        cheats = defaultdict(int)
+        dists = self.find_distances()
+        for pos, cost in dists.items():
+            for dest, destcost in dists.items():
+                md = pos.manhattan(dest)
+                if 0 < md <= cheatlen:
+                    savings = cost - destcost - md
+                    if savings > 0:
+                        cheats[savings] += 1
+        return cheats
+
+
+def part1(s):
+    g = Grid(s)
+    cheats = g.find_cheats1()
+    return sum(n for k, n in cheats.items() if k >= 100)
+
+
+def part2(s):
+    g = Grid(s)
+    cheats = g.find_cheats(20)
+    return sum(n for k, n in cheats.items() if k >= 100)
+
+
+def real_input():
+    with open("input.txt", "r") as infile:
+        return infile.read()
+
+
+def run_all():
+    print("Part 1 (1463)")
+    print(part1(real_input()))
+
+    print()
+    print("Part 2 (985332)")
+    print(part2(real_input()))
+
+
+if __name__ == "__main__":
+    run_all()
