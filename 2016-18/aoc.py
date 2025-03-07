65115707c46d6e9dab9e04f9f4410f5cbac5a6f6
Ryan D Johnson <ryandjohnson@gmail.com>
2025-03-06 17:01:45 -0800

    2016-18

---
+++ 2016-18/aoc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env pypy3
+
+ExamplesPart1 = (
+    ("..^^.\n", 3, 6),
+    (".^^.^.^^^^\n", 10, 38),
+)
+
+
+def part1(s, rows=40):
+    s = s.strip()
+
+    bitlen = len(s)
+    mask = (1 << bitlen) - 1
+
+    n = int(s.translate(str.maketrans(".^", "01")), 2)
+    bad = n.bit_count()
+    #print(format(n, f"0{bitlen}b"))
+    for _ in range(rows-1):
+        n = (n << 1 ^ n >> 1) & mask
+        #print(format(n, f"0{bitlen}b"))
+        bad += n.bit_count()
+
+    return bitlen * rows - bad
+
+
+def part2(s):
+    return part1(s, 400_000)
+
+
+def real_input():
+    with open("input.txt", "r") as infile:
+        return infile.read()
+
+
+def run_all():
+    for i, (a, rows, b) in enumerate(ExamplesPart1):
+        c = part1(a, rows)
+        check = "✅️" if b == c else "⚠️"
+        print(f"Example Part 1.{i} ({b})")
+        print(check, c)
+        print()
+
+    print("Part 1 (2035)")
+    print(part1(real_input()))
+    print()
+
+    print("Part 2")
+    print(part2(real_input()))
+    print()
+
+
+if __name__ == "__main__":
+    run_all()
