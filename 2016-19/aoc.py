ae23ed920c6512f767f0c27bed58a213ddf489ad
Ryan D Johnson <ryandjohnson@gmail.com>
2025-03-07 09:35:21 -0800

    2016-19 but math

--- 2016-19/aoc.py
+++ 2016-19/aoc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pypy3
 
 from collections import deque
+from math import log
 
 
 ExampleInput1 = """\
@@ -8,7 +9,7 @@ ExampleInput1 = """\
 """
 
 
-def part1(s):
+def part1sim(s):
     size = int(s.strip())
     d = deque(range(1, size+1))
 
@@ -20,7 +21,31 @@ def part1(s):
     return d[0]
 
 
-def part2(s):
+# The OEIS entry for part 2 referenced Josephus Problem:
+#
+#     https://en.wikipedia.org/wiki/Josephus_problem
+#     https://oeis.org/A006257
+#
+# The title of today's Avent of Code puzzle is An Elephant Named Joseph, so
+# that's definitely on purpose. This thing dates back to the first century CE.
+#
+# Anyway, might as well to a math version.
+#
+def part1math(s):
+    size = int(s.strip())
+    log2 = int(log(size, 2))
+    base = 2 ** log2
+    n = size - base
+    return 2*n + 1
+
+
+def part1(s):
+    return part1math(s)
+
+
+# I'm leaving the simulated version since it really did take some thought to
+# come up with this implementation that focuses on the center of the list.
+def part2sim(s):
     size = int(s.strip())
 
     front = deque(range(1, size//2 + 1))
@@ -40,6 +65,48 @@ def part2(s):
     return (front or back)[0]
 
 
+# I can't really explain this; I just printed out solutions and came up with a
+# closed form solution. The solutions reset back to 1 after powers of three,
+# then count up by one halfway to the next power of three, then count up by two
+# for the second half:
+#
+#    1:     1
+#
+#    2:     1
+#    3:     3
+#
+#    4:     1 (reset after 3**1, count by +1)
+#    5:     2
+#    6:     3
+#    7:     5 (second half, count by +2)
+#    8:     7
+#    9:     9
+#
+#    ... Up to 27 ...
+#    1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27
+#    ... after that it resets and builds back to 81, etc. ...
+#
+# Later, I checked OEIS and sure enough this sequence is documented as
+# "The n-cowboy shootout problem".
+#
+#     https://oeis.org/A334473
+#
+def part2math(s):
+    size = int(s.strip())
+    log3 = int(log(size, 3))
+    base = 3 ** log3
+    n = size - base
+    if n == 0:
+        return base
+    if n < base:
+        return n
+    return base + (n - base) * 2
+
+
+def part2(s):
+    return part2math(s)
+
+
 def real_input():
     with open("input.txt", "r") as infile:
         return infile.read()
@@ -55,7 +122,7 @@ def run_all():
 
     print()
     print("Example Part 2 (2)")
-    print(part2("5"))#ExampleInput1))
+    print(part2(ExampleInput1))
 
     print()
     print("Part 2 (1407007)")
