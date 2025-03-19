db5bab5a9a2c2f92b07dd0e1b2b2c7cea0c86f6c
Ryan D Johnson <ryandjohnson@gmail.com>
2025-03-18 17:37:07 -0700

    2015-01

---
+++ 2015-01/aoc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pypy3
+
+
+def ones(s):
+    return (1 if c == "(" else -1 for c in s.strip())
+
+
+def part1(s):
+    return sum(ones(s))
+
+
+def part2(s):
+    running = 0
+    for i, one in enumerate(ones(s)):
+        running += one
+        if running == -1:
+            return i + 1
+
+
+
+def real_input():
+    with open("input.txt", "r") as infile:
+        return infile.read()
+
+
+def run_all():
+    print("Part 1 (280)")
+    print(part1(real_input()))
+
+    print()
+    print("Part 2 (1797)")
+    print(part2(real_input()))
+
+
+if __name__ == "__main__":
+    run_all()
