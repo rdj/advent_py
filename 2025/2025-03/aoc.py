4d15986b78c5c30449bad8f4c9fe428512d8cf35
Ryan D Johnson <ryandjohnson@gmail.com>
2025-12-02 21:44:14 -0800

    2025-03 Part 2

--- 2025/2025-03/aoc.py
+++ 2025/2025-03/aoc.py
@@ -1,59 +1,5 @@
 #!/usr/bin/env python3
 
-## Things I used in previous years
-
-# from colors import color  ## pip install py-colors
-
-# from bitstring import Bits, BitStream
-# from collections import Counter
-# from collections import defaultdict
-# from collections import deque
-# from copy import deepcopy
-# from dataclasses import dataclass, astuple, replace
-# from enum import Enum, IntEnum, auto
-# from fractions import Fraction
-# from functools import cache, cached_property
-# from functools import cmp_to_key
-# from functools import lru_cache
-# from functools import partial
-# from functools import reduce
-# from heapq import heappush, heappop
-# from itertools import combinations
-# from itertools import count
-# from itertools import cycle
-# from itertools import groupby
-# from itertools import islice
-# from itertools import pairwise
-# from itertools import permutations
-# from itertools import product    ## Cartesian product
-# from math import log
-# from math import prod
-# from math import sqrt
-# from more_itertools import batched
-# from more_itertools import chunked
-# from more_itertools import ilen
-# from more_itertools import sliding_window
-# from multiprocessing import Pool
-# from numpy import transpose
-# from pathlib import Path
-# #from scipy.signal import convolve2d ## pypy3 unsupported
-# from typing import NamedTuple
-# import _md5
-# import cProfile
-# import functools as ft
-# import itertools as it
-# import json
-# import math
-# import networkx as nx
-# import numpy as np
-# import operator as op
-# import pygraphviz as pgv
-# import re
-# import regex # supports overlapped matches, recursive subpatterns (?<foo>...(?&foo)*...)
-# import sympy
-# import z3 # pip install z3-solver
-
-
 MultiLineExample = """\
 987654321111111
 811111111111119
@@ -78,22 +24,35 @@ def parse(s):
 
 
 def best_joltage(b):
-    for n1 in range(9, 0, -1):
-        try:
-            i = b[:-1].index(n1)
-            n2 = max(b[i+1:])
-            return n1 * 10 + n2
-        except:
-            continue
-    raise Exception("nope")
+    n1 = max(b[:-1])
+    i = b.index(n1)
+    n2 = max(b[i+1:])
+    return n1 * 10 + n2
 
 
 def part1(s):
     return sum(map(best_joltage, parse(s)))
 
 
+def best_joltage_recur(b, wanted, accum):
+    if wanted == 0:
+        return
+
+    if wanted == len(b):
+        accum += b
+        return
+
+    n = max(b[:-(wanted - 1)]) if wanted > 1 else max(b)
+    i = b.index(n)
+    accum.append(n)
+    best_joltage_recur(b[i+1:], wanted - 1, accum)
+
+
 def best_joltage2(b):
-    return 0
+    accum = []
+    best_joltage_recur(b, 12, accum)
+    j = int(''.join(map(str,accum)))
+    return j
 
 
 def part2(s):
@@ -124,7 +83,7 @@ def run_all():
         print(check, c)
         print()
 
-    print("Part 2 ()")
+    print("Part 2 (171975854269367)")
     print(part2(real_input()))
 
 
